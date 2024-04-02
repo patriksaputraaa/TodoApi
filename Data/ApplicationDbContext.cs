341ffa26e6636e1443998597617dff437f6e8a85
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-04-02 15:23:45 +0700

    Create Description

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,8 +5,15 @@ namespace TodoApi.Data;
 
 public class ApplicationDbContext: DbContext{
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options){
-
+        /*Ini adalah konstruktor untuk kelas ApplicationDbContext. 
+        Ini menerima objek DbContextOptions<ApplicationDbContext> 
+        yang menyediakan konfigurasi untuk koneksi ke database. 
+        Konstruktor ini kemudian meneruskan opsi tersebut ke konstruktor kelas dasar (base(options)), 
+        yang merupakan konstruktor kelas DbContext.*/
     }
     
     public DbSet<TodoItem> TodoItems{get; set;}
+    /*Ini adalah properti yang mewakili kumpulan objek TodoItem yang akan diakses atau dimanipulasi dalam database. 
+    Properti ini merupakan bagian dari kelas ApplicationDbContext dan akan digunakan untuk berinteraksi 
+    dengan tabel yang sesuai dengan model TodoItem dalam database.*/
 }
--- models/TodoCount.cs
+++ models/TodoCount.cs
@@ -8,5 +8,4 @@ public class TodoCount{
         Count = count;
         DataTodoItem = dataTodoItem;
     }
-
 }
